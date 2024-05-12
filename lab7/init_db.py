113d3372ec9b66f1c05b56c25b8f311ad6640d3c
Grena30 <grenadinu@yahoo.com>
2024-05-12 18:20:56 +0300

    feat: add intial server

---
+++ lab7/init_db.py
@@ -0,0 +1,12 @@
+from app import create_app, db, Courses
+
+
+def init_database():
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+
+
+if __name__ == "__main__":
+    init_database()
