09a329feec2cb87b55c33138fda3680a4b0e38a3
Dinu <grenadinu@yahoo.com>
2024-05-13 13:38:42 +0300

    feat: add backend server (#7)
    * feat: add intial server
    
    * feat: add CRUD API; add jwt authorization
    
    * feat: add swagger
    
    * feat: add pagination

---
+++ lab7/app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models.database import db
+from flask_swagger_ui import get_swaggerui_blueprint
+from models.courses import Courses
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+
+
+def authenticate(username, password):
+    users = {
+        'writer': 'writer',
+        'admin': 'admin'
+    }
+    if username in users and users[username] == password:
+        return username
+
+def identity(payload):
+    user_id = payload['identity']
+    return {'user_id': user_id}
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    db.init_app(app)
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.config['JWT_SECRET_KEY'] = 'secret'
+    jwt = JWTManager(app)
+    SWAGGER_URL = "/swagger"
+    API_URL = "/static/swagger.json"
+
+    swagger_ui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': 'Access API'
+        }
+    )
+    app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+    import routes
+    app.run(port=6060, debug=True)
---
+++ lab7/models/courses.py
@@ -0,0 +1,17 @@
+from models.database import db
+
+
+class Courses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    level = db.Column(db.Integer, nullable=False)
+    description2 = db.Column(db.String(255), nullable=False)
+    description3 = db.Column(db.String(255), nullable=False)
+    
+    def __init__(self, title, description, level, description2, description3):
+        self.title = title
+        self.description = description
+        self.level = level
+        self.description2 = description2
+        self.description3 = description3
